bc4c2292c0e5ff18ff3567d4b9fdb67e8bf696bd
sungjp <gpark18@amherst.edu>
2016-11-11 00:28:47 -0500

    edited

--- 1/1.py
+++ 1/1.py
@@ -1,22 +1,22 @@
 import itertools
 
-# Sum of any arithmetic progression is N(n1 + nN)/2 where N represents the number of elements to be summed, n1 is the first element of the series to be summed, and nN is the last element of the series to be summed.
-
-# A = Sum of 3 + 6 + ... + 999 = 333(3 + 999)/2
-# B = Sum of 5 + 10 + ... + 1000 = 199(5 + 995)/2
-# C = Sum of 15 + 30 + ... + 995 = 66(15 + 990)/2
-# Sum of multiples of 3 or 5 below 1000 = A + B - C
+"""
+ The sum of an arithmetic progression is N(F + L)/2 where N represents the number of elements to be summed, F is the first element of the series to be summed, and L is the last element of the series to be summed.
+ A = Sum of 3 + 6 + ... + 999 = 333(3 + 999)/2
+ B = Sum of 5 + 10 + ... + 995 = 199(5 + 995)/2
+ C = Sum of 15 + 30 + ... + 990 = 66(15 + 990)/2
+ Sum of multiples of 3 or 5 below 1000 = A + B - C
+"""
 
 print (333*(3 + 999)/2 + 199*(5 + 995)/2 - 66*(15 + 990)/2)
 
-# let's generalize this solution as a function to solve similar problems
-
+# generalizing
 def sum_mult(below,*multiples_of):
-    # handle edge case in which all the multiples are actually >= the constraint
+    # if all multiples are >= the constraint
     if len(list(filter(lambda x: x >= below, multiples_of))) == len(multiples_of):
         return 0
 
-    # auxillary fx for finding the sum of an arithmetic progression
+    # auxillary for progression sum
     def aux_ap(first):
         last = 0
         i = below - 1
